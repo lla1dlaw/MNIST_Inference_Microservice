68eee36a7d4528e0ebcc74b2b753cee8b787f0e6
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-04-15 12:32:00 -0400

    added functionality to save model dictionaries instead of just full models. Makes loading of models portable.

--- train_nets.py
+++ train_nets.py
@@ -99,7 +99,7 @@ def test_model(model, test_loader, is_cnn: bool):
         return acc
 
 
-def print_and_save(model, dimensions = None, is_cnn = False):
+def print_and_save(model, dimensions: list[int] = None, is_cnn: bool = False):
     if not is_cnn:
         dims = "-".join([str(x) for x in dimensions])
         filename = f"{dims}.pt"
@@ -114,7 +114,25 @@ def print_and_save(model, dimensions = None, is_cnn = False):
     if save:
         torch.save(model, os.path.join(save_path, filename))
         print(f"Model saved to: {save_path}")
-        
+
+
+def load_save_all_state_dicts():
+    load_dir = os.path.join("models")
+    save_dir = os.path.join("model_dicts")
+    os.makedirs(save_dir, exist_ok=True)
+    model = None
+
+    for filename in os.listdir(load_dir):
+        # get file paths
+        model_load_path = os.path.join(load_dir, filename)
+        filename_no_ext = os.path.splitext(filename)[0]
+        model_save_path = os.path.join(save_dir, f"{filename_no_ext}-dict.pt")
+
+        # load model and save its dicts (uses cpu becuase it is unknown)
+        model: nn.Module = torch.load(model_load_path, map_location=device, weights_only=False)
+        model.eval()
+        torch.save(model.state_dict(), model_save_path)
+
 
 def load_data(batch_size: int):
     # Load MNIST
@@ -169,6 +187,7 @@ def get_network_dimensions() -> list[int]:
     return dimensions
 
 
+
 def main():
     print("\n----------Welcome to the MNIST Neural Network Trainer----------\n")
     print("- This program trains a feed forward neural network on the MNIST dataset.")
@@ -214,6 +233,9 @@ def main():
         else:
             print_and_save(model, is_cnn=train_cnn)
 
+    save_full_models = pyip.inputYesNo("Save previous trained models? (y/n): ", yesVal='y', noVal='n') == 'y'
+    if save_full_models:
+        load_save_all_state_dicts()
 
 if __name__ == "__main__":
     main()
